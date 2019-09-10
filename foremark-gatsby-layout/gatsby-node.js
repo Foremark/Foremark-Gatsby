dd2246b3b4c1960d5fb430a03e2b8b62c0e61118
yvt <i@yvt.jp>
2019-09-10 10:02:16 +0200

    Define environmental variables using `DefinePlugin`

--- foremark-gatsby-layout/gatsby-node.js
+++ foremark-gatsby-layout/gatsby-node.js
@@ -1,4 +1,4 @@
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions, plugins }) => {
   // map `preact` to `react` that GatsbyJS uses
   actions.setWebpackConfig({
     resolve: {
@@ -6,5 +6,16 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
         preact: `react`,
       },
     },
+    plugins: [
+      plugins.define({
+        'process.env': {
+          'VERSION': JSON.stringify(require('./foremark/package.json').version + ' (GatsbyJS port)'),
+          'URL': JSON.stringify(require('./foremark/package.json').homepage),
+          'COMMITHASH': JSON.stringify('?'),
+          'BRANCH': JSON.stringify('?'),
+          'LAZY_LOADING': JSON.stringify(false),
+        },
+      })
+    ],
   });
 };
