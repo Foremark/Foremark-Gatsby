ed1a6b51d38c1f05eca6e49ef73d4ac72b6fb7ff
yvt <i@yvt.jp>
2020-01-31 13:21:55 +0900

    Update Foremark to 0.4.2

--- foremark-gatsby-transformer/src/diagram.ts
+++ foremark-gatsby-transformer/src/diagram.ts
@@ -24,36 +24,92 @@ const toSvgPromise = (async () => {
     const wasm = (module.instance as any).exports;
 
     let WASM_VECTOR_LEN = 0;
-    const cachedTextEncoder = new TextEncoder();
+    const lTextEncoder = typeof TextEncoder === 'undefined' ? require('util').TextEncoder : TextEncoder;
+    let cachedTextEncoder = new lTextEncoder('utf-8');
     const cachedTextDecoder = new TextDecoder();
 
-    function passStringToWasm(arg: string): number {
+    const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+        ? function (arg: any, view: any) {
+        return cachedTextEncoder.encodeInto(arg, view);
+    }
+        : function (arg: any, view: any) {
         const buf = cachedTextEncoder.encode(arg);
-        const ptr = wasm.__wbindgen_malloc(buf.length);
-        new Uint8Array(wasm.memory.buffer).set(buf, ptr);
-        WASM_VECTOR_LEN = buf.length;
+        view.set(buf);
+        return {
+            read: arg.length,
+            written: buf.length
+        };
+    });
+
+    let cachegetUint8Memory0: Uint8Array | null = null;
+    function getUint8Memory0() {
+        if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+            cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+        }
+        return cachegetUint8Memory0;
+    }
+
+    let cachegetInt32Memory0: Int32Array | null = null;
+    function getInt32Memory0() {
+        if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+            cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+        }
+        return cachegetInt32Memory0;
+    }
+
+    function passStringToWasm0(arg: string, malloc: Function, realloc: Function) {
+        if (realloc === undefined) {
+            const buf = cachedTextEncoder.encode(arg);
+            const ptr = malloc(buf.length);
+            getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
+            WASM_VECTOR_LEN = buf.length;
+            return ptr;
+        }
+
+        let len = arg.length;
+        let ptr = malloc(len);
+
+        const mem = getUint8Memory0();
+
+        let offset = 0;
+
+        for (; offset < len; offset++) {
+            const code = arg.charCodeAt(offset);
+            if (code > 0x7F) break;
+            mem[ptr + offset] = code;
+        }
+
+        if (offset !== len) {
+            if (offset !== 0) {
+                arg = arg.slice(offset);
+            }
+            ptr = realloc(ptr, len, len = offset + arg.length * 3);
+            const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+            const ret = encodeString(arg, view);
+
+            offset += ret.written;
+        }
+
+        WASM_VECTOR_LEN = offset;
         return ptr;
     }
 
-    function getStringFromWasm(ptr: number, len: number): string {
+    function getStringFromWasm0(ptr: number, len: number): string {
         return cachedTextDecoder
             .decode(new Uint8Array(wasm.memory.buffer).subarray(ptr, ptr + len));
     }
     return (diagram: string) => {
-        const ptr0 = passStringToWasm(diagram);
-        const len0 = WASM_VECTOR_LEN;
-        const retptr = wasm.__wbindgen_global_argument_ptr();
+        let r0: number = 0;
+        let r1: number = 0;
         try {
-            wasm.to_svg(retptr, ptr0, len0);
-            const mem = new Uint32Array(wasm.memory.buffer);
-            const rustptr = mem[retptr / 4];
-            const rustlen = mem[retptr / 4 + 1];
-
-            const realRet = getStringFromWasm(rustptr, rustlen).slice();
-            wasm.__wbindgen_free(rustptr, rustlen * 1);
-            return realRet;
+            const ptr0 = passStringToWasm0(diagram, wasm.__wbindgen_export_0, wasm.__wbindgen_export_1);
+            const len0 = WASM_VECTOR_LEN;
+            wasm.to_svg(8, ptr0, len0);
+            r0 = getInt32Memory0()[8 / 4 + 0];
+            r1 = getInt32Memory0()[8 / 4 + 1];
+            return getStringFromWasm0(r0, r1);
         } finally {
-            wasm.__wbindgen_free(ptr0, len0);
+            wasm.__wbindgen_export_2(r0, r1);
         }
     };
 })();
